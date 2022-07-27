a631bbb7814cd21257966fb65855011d4517759e
Eddie Casañas <107152796+Barriose01@users.noreply.github.com>
2022-07-27 07:55:25 -0700

    Add files via upload

--- adivina_numero.py
+++ adivina_numero.py
@@ -2,57 +2,60 @@
 # 30/05/2021
 # Santiago, Chile
 # Eddie Casañas
-
+#-----------------------------------------------------------------------#
 import random as rd
-
 
 def jugador():
 	nombre = input("\nDame tu nombre: ")
-	print("Bienvenido " + nombre.capitalize() + ". Adivina un numero del 1 al 20")
+	print("\nBienvenido " + nombre.capitalize() + ". Adivina un numero del 1 al 20")
 	print("Tienes 5 intentos")
+	print("Presione el numero cero (0) en cualquier momento para salir")
 	pc = rd.randint(1,20)
-	
 	n = 0
 	while n < 5:
-		
+		print("\nINTENTO #" + str(n + 1))
 		try:
 			opcion_j = int(input("\nDame un numero del 1 al 20: "))
 		except:
 			print("Por favor, ingrese un numero valido")
 			continue
 		else:
-		
-			if opcion_j not in range(1,21):
+			if opcion_j == 0:
+				break
+			elif opcion_j not in range(1,21):
 				print("Por favor, ingrese un numero entre el rango solicitado")
 				continue
-			
 			elif opcion_j < pc:
-				print("Un poco bajo. Intentalo de nuevo")
 				n += 1
+				if n != 5:
+					print("Un poco bajo. Intentalo de nuevo")
+				
 				continue
 			elif opcion_j > pc:
-				print("Un poco alto. Intentalo de nuevo")
 				n += 1
+				if n != 5:
+					print("Un poco alto. Intentalo de nuevo")
 				continue
 			if opcion_j == pc:
 				break
-			
-	if opcion_j == pc:
-		
+	if opcion_j == 0:
+		pass
+	elif opcion_j == pc:
 		print("\nFelicidades, adivinaste el numero en " + str(n + 1) + " intentos")
-		pregunta()
-		
 	else:
 		print("\nNo adivinaste el numero. El numero correcto era " + str(pc))
-		n = 0
-		pregunta()
 
 def pregunta():
-	pregunta = input("¿Deseas volver a jugar? (y/n): ")
-	if pregunta.strip().lower() == "y":
+	print("\n(1): Jugar")
+	print("(2): Salir")
+	opcion = input().lower().strip()
+	return opcion
+
+while True:
+	opcion = pregunta()
+	if opcion == "1":
 		jugador()
+	elif opcion == "2":
+		break
 	else:
-		pass
-			
-			
-jugador()
+		print("Ingrese una opcion valida")
